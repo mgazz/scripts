aae33784fabc45fea12e49e97f9589d304ab5d46
Michele <michele.gazzetti@gmail.com>
2016-04-14 15:56:18 +0200

    com

---
+++ vagrant/install-vagrant.sh
@@ -0,0 +1,7 @@
+wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+sudo dpkg -i vagrant_1.8.1_x86_64.deb
+vagrant box add ubuntu/trusty64 https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
+
+#mkdir vagrant_project
+#cd vagrant_project
+#vagrant init ubuntu/trusty64; vagrant up --provider virtualbox
---
+++ virtualbox/virtualbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -       
+#description     :virtualbox install script
+#author          :Michele Gazzetti
+#date            :14.04.16
+#notes           :
+#==============================================================================
+
+### Variables
+
+### Script start
+
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y virtualbox-4.3
+sudo apt-get install -y virtualbox-guest-additions-iso
