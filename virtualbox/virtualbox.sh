cd735d22eec8b3fbe109efc9b77df9978181e5fe
Michele <michele.gazzetti@gmail.com>
2016-04-16 10:07:34 +0200

    com

--- virtualbox/virtualbox.sh
+++ virtualbox/virtualbox.sh
@@ -9,8 +9,9 @@
 
 ### Script start
 
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+#sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
+#wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y virtualbox-4.3
+#sudo apt-get install -y virtualbox-4.3
+sudo apt-get install -y virtualbox
 sudo apt-get install -y virtualbox-guest-additions-iso
