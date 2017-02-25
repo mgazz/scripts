c96f74e548305c751156070020ecda12aaff7db9
Michele Gazzetti <mgazzett@ie.ibm.com>
2017-02-25 16:05:32 +0000

    vim installation properly defined

---
+++ sys-setup/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -       
+#description     :
+#author          :Michele Gazzetti
+#date            :25.02.17
+#notes           :
+#==============================================================================
+sudo apt-get update
+
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get  update
+#
+
+sudo apt-get install rxvt-unicode-256color \
+    i3 \
+    scrot \
+    tmux \
+    spotify-client \
+    google-chrome-stable \
+    htop \
+    python-dev \
+    cmake \
+    libncurses-dev \
+    vim \
+    git -y
+
+git clone https://github.com/mgazz/dotfiles.git /home/$USER/dotfiles
+make -C /home/$USER/dotfiles
+
+git clone https://github.com/mgazz/scripts.git /home/$USER/scripts
+/home/$USER/scripts/sys-setup/vim.sh
+
+
--- sys-setup/vim.sh
+++ sys-setup/vim.sh
@@ -1,4 +1,47 @@
-cd /tmp
-git clone https://github.com/vim/vim.git
-./configure --prefix=/home/$USER     --enable-gui=gnome2     --with-features=huge     --enable-cscope     --enable-multibyte     --enable-rubyinterp     --enable-pythoninterp     --with-tlib=ncurses     --enable-luainterp
-make install
+#!/bin/bash -       
+#description     :
+#author          :Michele Gazzetti
+#date            :25.02.17
+#notes           :
+#==============================================================================
+
+
+#python different lib path based on different architectures
+ARC=`uname -a | awk '{print $12}'`
+if [ "$ARC" =  "x86_64" ]; then
+	PY_CONF_DIR="/usr/lib/python2.7/config-x86_64-linux-gnu"
+fi
+if [ "$ARC" = "i686" ]; then
+	PY_CONF_DIR="/usr/lib/python2.7/config-i386-linux-gnu"
+fi
+echo $PY_CONF_DIR
+
+mkdir -p /home/$USER/local
+
+git clone https://github.com/vim/vim.git /tmp/vim
+
+/tmp/vim/configure --prefix=/home/$USER/local \
+    --enable-gui=gnome2 \
+    --with-features=huge \
+    --enable-cscope \
+    --enable-multibyte \
+    --enable-luainterp \
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --with-python-config-dir=$PY_CONF_DIR \
+    --with-tlib=ncurses 
+
+make install -C /tmp/vim/
+
+mkdir -p ~/.vim/bundle
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sed -i 's/colorscheme/"colorscheme/' ~/.vimrc
+
+vim -c "call CommandLinePluginInstall()"
+
+~/.vim/bundle/YouCompleteMe/install.py
+
+#re-activate colorscheme
+sed -i 's/"colorscheme/colorscheme/' ~/.vimrc
